c63e938d990098d0567c89f3e34170fd7ec15920
zengkm <zengkm3@mail2.sysu.edu.cn>
2017-03-28 13:59:13 +0800

    user ffaker to generate 200 page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328030635) do
+ActiveRecord::Schema.define(version: 20170328055520) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
