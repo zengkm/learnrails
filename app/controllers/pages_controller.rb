863381b10e9306303736b844d3cd113f615993a2
zengkm <zengkm3@mail2.sysu.edu.cn>
2017-03-28 15:22:04 +0800

    upload image by using carrierwave

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -69,6 +69,6 @@ class PagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
-      params.require(:page).permit(:title, :content)
+      params.require(:page).permit(:title, :content, :image)
     end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,3 @@
 class Page < ApplicationRecord
+	mount_uploader :image, PageImageUploader
 end
